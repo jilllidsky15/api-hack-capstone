31ce55f7e8c9111d4ed31c9a1cf455791a32acf8
Jill Lidsky <jilllidsky15@gmail.com>
2020-02-23 14:04:44 -0500

    Changed API back to LocationIQ, fixed issues with showing errors/results, added more parameters to hiking URL

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const locationKey = '05f174bb5a634cfeb6111ff86e830812';
-const locationURL = 'https://api.opencagedata.com/geocode/v1/json';
+const locationKey = 'c4314e461218a7';
+const locationURL = 'https://us1.locationiq.com/v1/search.php';
 
 const hikingKey = '200683797-e94ac7495e9e689dc27179e7604e5453';
 const hikingURL = 'https://www.hikingproject.com/data/get-trails';
 
 function displayResults(responseJson) {
-  $(".results-list").empty();
+  $('.results-list').empty();
   for (let i = 0; i < responseJson.trails.length; i++) {
     $('.results-list').append(`
     <li class="item-container">
@@ -24,17 +24,17 @@ function displayResults(responseJson) {
   $('.results').removeClass('hidden');
 }
 
-function createHikingURL(lat, lng) {
-  return `${hikingURL}?lat=${lat}&lon=${lng}&key=${hikingKey}`;
+function createHikingURL(lat, lon) {
+  return `${hikingURL}?lat=${lat}&lon=${lon}&maxResults=30&maxDistance=25&key=${hikingKey}`;
 }
 
 // Pull lat and lng coordinates from json object
 function useCoordinates(responseJson) {
   // console.log(responseJson);
-  if (responseJson.results.length > 0) {
-    const lat = responseJson.results[0].geometry.lat;
-    const lng = responseJson.results[0].geometry.lng;
-    const fullHikingURL = createHikingURL(lat, lng);
+  if (responseJson.length > 0) {
+    const lat = responseJson[0].lat;
+    const lon = responseJson[0].lon;
+    const fullHikingURL = createHikingURL(lat, lon);
     // console.log(fullHikingURL);
 
     fetch(fullHikingURL)
@@ -54,8 +54,9 @@ function useCoordinates(responseJson) {
 function getCoordinates(queryPostalcode) {
   const locationParams = {
     key: locationKey,
-    q: queryPostalcode,
-    countrycode: "us",
+    postalcode: queryPostalcode,
+    countrycodes: "us",
+    format: "json",
     limit: 1,
   }
   // console.log(locationParams);
@@ -73,7 +74,7 @@ function getCoordinates(queryPostalcode) {
     })
     .then(responseJson => useCoordinates(responseJson))
     .catch(err => {
-      console.log(err);
+      // console.log(err);
       $('.js-error-message').removeClass('hidden').text(`Sorry! We couldn't find any trails matching your request!`);
     });
 }
@@ -88,8 +89,9 @@ function formatLocationParams(locationParams) {
 function watchForm() {
   $('form').submit(function (event) {
     event.preventDefault();
-    $('.js-error-message').addClass('hidden');
     // What did the user input? Get the value of their input
+    $('.results').addClass('hidden');
+    $('.js-error-message').addClass('hidden');
     const inputPostalcode = $('.postalcode').val();
     // console.log(inputPostalcode);
     getCoordinates(inputPostalcode);
